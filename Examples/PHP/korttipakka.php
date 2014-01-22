d22d228ed2921fa7e1e1919652dbc09dca4a96cd
Nina <nina.ranta@hotmail.com>
2014-01-22 15:10:45 +0200

    Update korttipakka.php

--- Examples/PHP/korttipakka.php
+++ Examples/PHP/korttipakka.php
@@ -1,10 +1,10 @@
 <?php
 
-/* Tarkoituksena on luoda korttipakka, josta randomilla nostetaan joku kortti ja sen jälkeen
-poistetaan se pinkasta. Pakkaa pyöritellään läpi niin kauan aikaa, kunnes kaikki kortit on nostettu */
+/* Tarkoituksena on luoda korttipakka, josta randomilla nostetaan joku kortti ja sen jÃ¤lkeen
+poistetaan se pinkasta. Pakkaa pyÃ¶ritellÃ¤Ã¤n lÃ¤pi niin kauan aikaa, kunnes kaikki kortit on nostettu */
 
 
-		// Määritellään kaikki kortit taulukkoon mitä pakassa on
+		// MÃ¤Ã¤ritellÃ¤Ã¤n kaikki pakan kortit tauluun
 		$cards = array('spades1', 'spades2', 'spades3', 'spades4', 'spades5', 'spades6', 'spades7', 'spades8', 'spades9', 'spades10', 'spades11', 'spades12', 'spades13', 'club1', 'club2', 'club3', 'club4', 'club5', 'club6', 'club7', 'club8', 'club9', 'club10', 'club11', 'club12', 'club13', 'hearts', 'hearts2', 'hearts3', 'hearts4', 'hearts5', 'hearts6', 'hearts7', 'hearts8', 'hearts9', 'hearts10', 'hearts11', 'hearts12', 'hearts13', 'diamond1', 'diamond2', 'diamond3', 'diamond4', 'diamond5', 'diamond6', 'diamond7', 'diamond8', 'diamond9', 'diamond10', 'diamond11', 'diamond12', 'diamond13');
 
 		do {
@@ -17,12 +17,11 @@ poistetaan se pinkasta. Pakkaa pyöritellään läpi niin kauan aikaa, kunnes kaikki
 			unset($cards[0]);
 
 			// Tulostetaan randomoitu kortti
-			echo "Nostettu kortti: " . $randomCard . "<br>";
+			echo "Nostettu kortti: " . $randomCard;
 
-			// Tulostetaan kuinka monta korttia on jäljellä
-			echo $randomCard . " poisettiin pakasta" . "<br>" . "<br>";
-			echo "Kortteja jäljellä: " . intval(count($cards)) . "<br>" . "<br>";
-
+			// Tulostetaan kuinka monta korttia on jÃ¤ljellÃ¤
+			echo $randomCard . " poisettiin pakasta";
+			echo "Kortteja jï¿½ljellï¿½: " . intval(count($cards));
 
 		} while ($cards != NULL)
 						
