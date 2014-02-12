6dcd7df3177f4704d17e298069706dba2f006323
Nina <nina.ranta@hotmail.com>
2014-02-12 14:57:12 +0200

    edit
    edit

---
+++ Java/Programs/Demo.java
@@ -0,0 +1,25 @@
+// Class name
+public class Demo {
+	
+	// function for result and returning it
+	public static int summation(int n) {
+		// result formula
+		int result = (n *(n + 1)) / 2;
+		
+		// returning result value
+		return result;	
+	}
+	
+	// main function where everything happens
+	public static void main(String[] args) {
+		int input = 3;
+		
+		// summation(3);
+		int finalResult = summation(input);
+		
+		// prints out (3*(3+1)) / 2 = 6
+		System.out.print(finalResult);
+	}
+
+}
+ 
