ef8a4b926065f5f75587f705b70dc4d08a00415b
Nina <nina.ranta@hotmail.com>
2014-02-14 15:28:39 +0200

    Update footer.php

--- cms/templates/include/footer.php
+++ cms/templates/include/footer.php
@@ -1,5 +1,5 @@
 	<div id = "footer">
-			Nina Ranta 2014. All rights reserved. <a href ="http://www.jmdprojects.net/tagfun/">TagFun</a>
+			Nina 2014. All rights reserved. <a href ="http://www.jmdprojects.net/blog-tagfun/">TagFun</a>
 		</div>
 		</div>
 	</body>
